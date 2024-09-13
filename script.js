a18b8dcb49acf1e7081324ebbbebb401f9ae4533
aabdslvm <118897441+aabdslvm@users.noreply.github.com>
2024-09-13 17:28:31 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,82 @@
+let computerMove = '';
+
+let result = '';
+
+let score = {
+    wins: 0,
+    losses: 0,
+    ties: 0
+}
+
+function computerPlay() {
+    const randomNumber = Math.random();
+
+        if (randomNumber >= 0 && randomNumber < 1/3) {
+            computerMove = 'rock'  
+            
+        }
+        else if (randomNumber >= 1/3 && randomNumber < 2/3) {
+            computerMove = 'paper'
+            
+        }
+        else if (randomNumber >= 2/3 && randomNumber < 1) {
+            computerMove = 'scissors'
+            
+        }
+        console.log(`computer randomly picked: ${computerMove}`);
+}
+
+function play(userMove) {
+
+    if (userMove === 'rock') {
+
+        if (computerMove === 'rock') {
+            result = `computer picked ${computerMove}, you picked ${userMove}, tie`
+        }
+
+        else if (computerMove === 'paper') {
+            result = `computer picked ${computerMove}, you picked ${userMove}, you lose`
+        }
+
+        else if (computerMove === 'scissors') {
+            result = `computer picked ${computerMove}, you picked ${userMove}, you win`
+        }
+    } else if (userMove === 'paper') {
+
+        if (computerMove === 'rock') {
+            result = `computer picked ${computerMove}, you picked ${userMove}, you win`
+        }
+
+        else if (computerMove === 'paper') {
+            result = `computer picked ${computerMove}, you picked ${userMove}, tie`
+        }
+
+        else if (computerMove === 'scissors') {
+            result = `computer picked ${computerMove}, you picked ${userMove}, you lose`
+        }
+
+    } else if (userMove === 'scissors') {
+
+        if (computerMove === 'rock') {
+            result = `computer picked ${computerMove}, you picked ${userMove}, you lose`
+        }
+
+        else if (computerMove === 'paper') {
+            result = `computer picked ${computerMove}, you picked ${userMove}, you win`
+        }
+
+        else if (computerMove === 'scissors') {
+            result = `computer picked ${computerMove}, you picked ${userMove}, tie`
+        }
+    }
+
+    alert(`${result}
+Wins: ${score.wins}, Losses: ${score.losses}, Ties: ${score.ties}`);
+}
+if (result.includes('win')) {
+    score.wins += 1;
+} else if (result.includes('lose')) {
+    score.losses += 1;
+} else if (result.includes('tie')) {
+    score.ties += 1;
+}
